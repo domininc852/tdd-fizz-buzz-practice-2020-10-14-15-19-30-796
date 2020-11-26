d8d563bdf10dc5ae678a3f63e249d8865a4b6d58
NSC/Network Service Center <NSC@oocl.com>
2020-11-26 14:00:53 +0800

    [fizz-buzz][Dominic Leung] test: implement normal case test

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_string_order_number_when_count_off_given_order_number(){
+        //given
+        int order=1;
+        FizzBuzz fizzbuzz=new FizzBuzz();
+        //when
+        String actualOrder=fizzbuzz.countOff(order);
+        //then
+        assertEquals("1",actualOrder);
+    }
 }
