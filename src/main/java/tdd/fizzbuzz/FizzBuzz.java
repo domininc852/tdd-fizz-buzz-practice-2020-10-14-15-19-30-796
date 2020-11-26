1bb75fd4100bc6144c1d0cd19c5c45811b07d13b
NSC/Network Service Center <NSC@oocl.com>
2020-11-26 14:13:31 +0800

    [fizz-buzz][Dominic Leung] refactor: extract BUZZ and BUZZ_NUMBER

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,13 +2,15 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     private final String FIZZ="Fizz";
+    private final String BUZZ="Buzz";
     private final int FIZZ_NUMBER=3;
+    private final int BUZZ_NUMBER=5;
     public String countOff(int order) {
         if (order%FIZZ_NUMBER==0){
             return FIZZ;
         }
-        else if (order%5==0){
-            return "Buzz";
+        else if (order%BUZZ_NUMBER==0){
+            return BUZZ;
         }
         return String.valueOf(order);
     }
